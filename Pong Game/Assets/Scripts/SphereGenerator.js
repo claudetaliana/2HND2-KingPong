f669212560be381ba747b64262e704072ae06aef
claudetaliana <talianaclaude3@gmail.com>
2014-02-18 13:23:28 +0100

    Added Unity Project
    Added all files

---
+++ Pong Game/Assets/Scripts/SphereGenerator.js
@@ -0,0 +1,53 @@
+﻿#pragma strict
+//a rigid body that links to the sphere prefab
+//Red Sphere
+var redsphere:Rigidbody;
+//Green Sphere
+var greensphere:Rigidbody;
+//Blue Sphere
+var bluesphere:Rigidbody;
+//Yellow Sphere
+var yellowsphere:Rigidbody;
+
+function generatesphere(){
+	//create a random number from 1 to 4
+	var colornumber:int;
+	
+	colornumber = Random.Range(1,5);
+	
+	Debug.Log(colornumber);
+	
+	//create a random number for the position
+	var xposition:float;
+	
+	xposition = Random.Range(-2.5f, 2.5f);
+	
+	Debug.Log(xposition);
+	
+	switch(colornumber)
+	{
+		case 1:
+				Instantiate(redsphere,Vector3(xposition,5,0),Quaternion.identity);
+				break;
+		case 2:
+				Instantiate(greensphere,Vector3(xposition,5,0),Quaternion.identity);
+				break;
+		case 3:
+				Instantiate(bluesphere,Vector3(xposition,5,0),Quaternion.identity);
+				break;
+		case 4:
+				Instantiate(yellowsphere,Vector3(xposition,5,0),Quaternion.identity);
+				break;		
+	}
+}
+
+
+function Start () {
+	//run the generate sphere method. first parameter is the startup delay, and the 
+	//second parameter is how often it will run
+	InvokeRepeating("generatesphere",1.0,1.0);
+}
+
+function Update () {
+
+}
---
+++ Pong Game/Assets/Scripts/SphereMovement.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+var spherespeed:int;
+
+
+function Start () {
+
+}
+
+function Update () {
+	transform.Translate(Vector3.up * -1 * spherespeed * Time.deltaTime);
+}
